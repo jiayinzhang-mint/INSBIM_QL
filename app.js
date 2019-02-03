08f8a44232742b5124d682902c143ecf6672baa8
MINT <mint@inslens.com>
2019-02-03 23:41:09 +0800

    db setup

--- app.js
+++ app.js
@@ -3,14 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// mongoose
+const mongoose = require("mongoose");
+
+// const configDev = [];
+// configDev.usr = "mint";
+// configDev.pwd = "INSCHINAisdead1";
+// const url = `mongodb://${configDev.usr}:${configDev.pwd}@39.96.61.110/INSLENS`;
+const urlBase = 'mongodb://127.0.0.1/INSBIM'
+
+mongoose.connect(
+  urlBase,
+  { useNewUrlParser: true, useFindAndModify: false }
+);
+const db = mongoose.connection;
+db.on("open", () => {
+  console.log("MongoDB Connection Success");
+});
+db.on("error", () => {
+  console.log("MongoDB Connection Error");
+});
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,12 +40,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
