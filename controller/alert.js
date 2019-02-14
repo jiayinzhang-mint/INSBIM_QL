93955a424a1f2c110ead8e0a0beecae9575f0c2c
MINT <mint@inslens.com>
2019-02-14 15:38:00 +0800

    alert init

--- controller/alert.js
+++ controller/alert.js
@@ -1,1 +1,10 @@
 import createError from "http-errors";
+import Alert from "../models/alert";
+
+class alertController {
+  static async createAlert(req, res, next) {}
+  static async getAlert(req, res, next) {}
+  static async updateAlert(req, res, next) {}
+  static async deleteAlert(req, res, next) {}
+}
+export default alertController;
--- routes/alert.js
+++ routes/alert.js
@@ -4,4 +4,32 @@ import auth from "../utils/auth";
 import alertController from "../controller/alert";
 var router = Router();
 
+router.post("/", auth.required, async (req, res, err) => {
+  if (!role.isAdmin(req.payload.username)) {
+    return createError(403, "No permission.");
+  }
+  alertController.createAlert(req, res, err);
+});
+
+router.get("/", auth.required, async (req, res, err) => {
+  if (!role.isAdmin(req.payload.username)) {
+    return createError(403, "No permission.");
+  }
+  alertController.getAlert(req, res, err);
+});
+
+router.put("/", auth.required, async (req, res, err) => {
+  if (!role.isAdmin(req.payload.username)) {
+    return createError(403, "No permission.");
+  }
+  alertController.updateAlert(req, res, err);
+});
+
+router.delete("/", auth.required, async (req, res, err) => {
+  if (!role.isAdmin(req.payload.username)) {
+    return createError(403, "No permission.");
+  }
+  alertController.deleteAlert(req, res, err);
+});
+
 export default router;
